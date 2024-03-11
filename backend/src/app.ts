66ca3a9cb33cb9d54e6f99b27d0bab9d0aaaa909
Raphaël Lemeyeur <78013277+raphael-lemeyeur@users.noreply.github.com>
2024-03-11 15:36:18 +0100

    Refactor and add default port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,45 +1,56 @@
-import express, {Request, Response} from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import session from 'express-session';
-import {AccountRoute} from "./account/AccountRoute";
-import {errorHandlerMiddleware} from "./displayableErrors/ErrorHandlerMiddleware";
-import {ParsingResponseBodyMiddleware} from "./ParsingResponseBodyMiddleware";
-import {initPassport} from "./PassportAuthMiddleware";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import { AccountRoute } from "./account/AccountRoute";
+import { errorHandlerMiddleware } from "./displayableErrors/ErrorHandlerMiddleware";
+import { ParsingResponseBodyMiddleware } from "./ParsingResponseBodyMiddleware";
+import { initPassport } from "./PassportAuthMiddleware";
 import passport from "passport";
-import {AlbumRoute} from "./album/AlbumRoute";
+import { AlbumRoute } from "./album/AlbumRoute";
 
-dotenv.config({path: '../.env'});
-const port = process.env.PORT;
+dotenv.config({ path: "../.env" });
+const port = process.env.PORT ?? 3000;
 const app = express();
 
 // adding body parsing middleware to extract the payload of the request
 app.use(express.json());
+
 // middleware to allow CORS
 app.use(cors());
+
 app.use(ParsingResponseBodyMiddleware);
-app.use(session({
-  secret: "This is a secret",
-  resave: false,
-  saveUninitialized: false
-}));
+
+app.use(
+  session({
+    secret: "This is a secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 initPassport(app);
 
 const accountRoute = new AccountRoute();
 const albumRoute = new AlbumRoute();
 
-app.use('/api/accounts', accountRoute.getRouter());
-app.use('/api/albums', albumRoute.getRouter());
+app.use("/api/accounts", accountRoute.getRouter());
+app.use("/api/albums", albumRoute.getRouter());
 
-app.post('/api/login', passport.authenticate('local'), async (req: Request, res: Response) => {
-  res.status(200).json("logged in");
-});
+app.post(
+  "/api/login",
+  passport.authenticate("local"),
+  async (req: Request, res: Response) => {
+    res.status(200).json(req.user);
+  }
+);
 
-app.get('/', (req, res) => {
-  res?.send('Hello World!');
+app.get("/", (req, res) => {
+  res?.send({
+    message: "Hello World",
+  });
 });
 
 app.use(errorHandlerMiddleware);
 app.listen(port, () => {
-  console.log(`Express is listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
