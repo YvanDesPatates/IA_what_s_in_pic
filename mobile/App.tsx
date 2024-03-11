b6f4c65bddb86443e96d8453e12aab443d9359ad
Raphaël Lemeyeur <78013277+raphael-lemeyeur@users.noreply.github.com>
2024-03-11 20:58:18 +0100

    mobile: lock screen to portrait

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,8 +7,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import NavigationStack from './src/components/Navigation/NavigationStack';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from './src/services/api';
+import Orientation from 'react-native-orientation-locker';
 
 const App = () => {
+    Orientation.lockToPortrait();
+
     return (
         <QueryClientProvider client={queryClient}>
             <Provider store={store}>
