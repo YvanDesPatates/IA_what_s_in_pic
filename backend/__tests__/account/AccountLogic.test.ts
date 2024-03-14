ebeb972c45c198113b517f124465ad8837b338bb
Yvan Roux <yvis99@gmail.com>
2024-03-14 17:03:28 +0100

    fix some tests

--- backend/__tests__/account/AccountLogic.test.ts
+++ backend/__tests__/account/AccountLogic.test.ts
@@ -1,9 +1,9 @@
 import {AccountLogic} from "../../src/account/AccountLogic";
 import {DisplayableJsonError} from "../../src/displayableErrors/DisplayableJsonError";
-import {AccountJsonDAO} from "../../src/account/accountJsonDAO";
 import {MissingAttributeError} from "../../src/displayableErrors/MissingAttributeError";
+import {AccountRestdbDAO} from "../../src/account/AccountRestdbDAO";
 
-let mockFunctions: Array<jest.SpyInstance> = []
+const mockFunctions: Array<jest.SpyInstance> = [];
 
 describe("AccountLogic.ts tests", () => {
     beforeAll(() => {
@@ -17,35 +17,35 @@ describe("AccountLogic.ts tests", () => {
     });
 
     test("create account with unused email", async () => {
-        const idExistsMock = mockDaoIdExists((id) => false);
-        const accountToCreate = new AccountLogic("emaileeeee", "name", 5, "pwd");
+        const idExistsMock = mockDaoIdExists(async () => false);
+        const accountToCreate = await new AccountLogic("emaileeeee", "name", "pwd");
         const createdAccount = accountToCreate.create();
         expect(createdAccount).toBeInstanceOf(AccountLogic);
 
         idExistsMock.mockReset();
-    })
+    });
 
     test("create account without pwd is impossible", async () => {
-        expect(() => new AccountLogic("email", "name", 5).create())
+        expect(async () => await new AccountLogic("email", "name").create())
             .toThrow(MissingAttributeError);
     });
 
     test("create account with blank pwd is impossible", async () => {
-        expect(() => new AccountLogic("email", "name", 5, "").create())
+        expect(async () => await new AccountLogic("email", "name", "").create())
             .toThrow(DisplayableJsonError);
     });
 
     test("create account with existing email is impossible", async () => {
         const idExistsMock = mockDaoIdExists();
-        expect(() => new AccountLogic("email", "name", 5, "pwd").create())
+        expect(async () => await new AccountLogic("email", "name", "pwd").create())
             .toThrow(DisplayableJsonError);
 
         idExistsMock.mockReset();
     });
 
     test("update account", async () => {
-        const idExistsMock = mockDaoIdExists(id => id === "email");
-        const updatedAccount = new AccountLogic("newEmail", "name", 5, "pwd").update("email");
+        const idExistsMock = mockDaoIdExists(async id => id === "email");
+        const updatedAccount = await new AccountLogic("newEmail", "name", "pwd").update("email");
         expect(updatedAccount).toBeInstanceOf(AccountLogic);
         expect(updatedAccount).toHaveProperty("email", "newEmail");
 
@@ -54,15 +54,15 @@ describe("AccountLogic.ts tests", () => {
 
     test("update account with existing email is impossible", async () => {
         const idExistsMock = mockDaoIdExists();
-        expect(() => new AccountLogic("newEmail", "name", 5, "pwd").update("email"))
+        expect(async () => await new AccountLogic("newEmail", "name", "pwd").update("email"))
             .toThrow(DisplayableJsonError);
 
         idExistsMock.mockReset();
     });
 
     test("update account without pwd is impossible", async () => {
-        const idExistsMock = mockDaoIdExists(id => id === "email");
-        expect(() => new AccountLogic("newEmail", "name", 5).update("email"))
+        const idExistsMock = mockDaoIdExists(async id => id === "email");
+        expect(async () => await new AccountLogic("newEmail", "name").update("email"))
             .toThrow(MissingAttributeError);
 
         idExistsMock.mockReset();
@@ -70,14 +70,14 @@ describe("AccountLogic.ts tests", () => {
 
     test("delete account", async () => {
         const idExistsMock = mockDaoIdExists();
-        new AccountLogic("newEmail", "name", 5, "pwd").delete();
+        await new AccountLogic("newEmail", "name", "pwd").delete();
 
         idExistsMock.mockReset();
     });
 
     test("delete account with non existing email throw 404 error", async () => {
-        const idExistsMock = mockDaoIdExists(id => false);
-        expect(() => new AccountLogic("newEmail", "name", 5, "pwd").delete())
+        const idExistsMock = mockDaoIdExists(async () => false);
+        expect(async () => await new AccountLogic("newEmail", "name", "pwd").delete())
             .toThrow(DisplayableJsonError);
 
         idExistsMock.mockReset();
@@ -90,25 +90,21 @@ describe("AccountLogic.ts tests", () => {
  * Mock the idExists method of AccountJsonDAO
  * @param mockFunction is the return value the mocked function will return, by default it returns true
  */
-function mockDaoIdExists(mockFunction: (id: string) => boolean = (id) => true) {
+function mockDaoIdExists(mockFunction: (id: string) => Promise<boolean> = async () => true) {
     return jest
-        .spyOn(AccountJsonDAO.prototype, 'idExists')
+        .spyOn(AccountRestdbDAO.prototype, 'idExists')
         .mockImplementation(mockFunction);
 }
 
 function mockDaocreate() {
     return jest
-        .spyOn(AccountJsonDAO.prototype, 'create')
-        .mockImplementation((account) => {
-            return account;
-        });
+        .spyOn(AccountRestdbDAO.prototype, 'create')
+        .mockImplementation(async (account) => account);
 }
 
 function mockDaoDelete(returnValue: boolean = true) {
     return jest
-        .spyOn(AccountJsonDAO.prototype, 'delete')
-        .mockImplementation(() => {
-            return returnValue;
-        });
+        .spyOn(AccountRestdbDAO.prototype, 'delete')
+        .mockImplementation(async () => returnValue);
 }
 //#endregion
--- backend/__tests__/app.test.ts
+++ backend/__tests__/app.test.ts
@@ -4,9 +4,11 @@ import app from "../src/app";
 
 describe("Test app.ts", () => {
     test("Catch-all route", async () => {
-        const res = await request(app).get("/")
-            .expect("Content-Type", /text/)
+        await request(app).get("/")
+            .expect("Content-Type", /json/)
             .expect(200)
-            .expect(new RegExp("^Hello World!$"));
+            .expect({
+                message: "Hello World",
+            });
     });
 });
