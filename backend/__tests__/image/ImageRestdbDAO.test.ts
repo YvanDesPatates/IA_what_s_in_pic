e804d148b3b847a0bbc2e09e0e5f62bc461f7216
ROUX YVAN <yvan.roux@inrae.fr>
2024-03-15 14:40:37 +0100

    test for dao. Tests are commented because it is manual test, we dont want to push to the database everytime we run tests.

---
+++ backend/__tests__/image/ImageRestdbDAO.test.ts
@@ -0,0 +1,44 @@
+import {ImageDBModel} from "../../src/image/ImageDBModel";
+import fs from "fs";
+import {ImageRestdbDAO} from "../../src/image/ImageRestdbDAO";
+
+describe("AccountLogic.ts tests", () => {
+
+    test("fake test to avoid errors", async () => {
+        expect(true).toBe(true);
+    })
+
+    // test("create image save it in /imgs", async () => {
+    //     const imageBuffer = fs.readFileSync("/home/yvan/epitech/T-DEV-800-MPL_10/backend/__tests__/image/test_photo.png");
+    //     const imageBytes = getImageBytesFromBuffer(imageBuffer);
+    //     const imageDbModel = new ImageDBModel(imageBytes, "name", new Date().toDateString(), "Le y", [], []);
+    //     const createdImage = await new ImageRestdbDAO().create(imageDbModel);
+    //     if (!createdImage.id){
+    //         throw new Error("created image should have an id");
+    //     }
+    //     const gettedImage = await new ImageRestdbDAO().getById(createdImage.id);
+    //     if ( !gettedImage || !gettedImage.imageBytes){
+    //         throw new Error("getted image should have a buffer");
+    //     }
+    //     const bufferToWrite = getBufferFromBytes(gettedImage.imageBytes);
+    //     fs.writeFileSync("__tests__/image/saved_image_from_test.webp" ,bufferToWrite);
+    // });
+
+});
+
+function getImageBytesFromBuffer(buffer: Buffer): number[] {
+    const imageBytes = [];
+    for (let byte = 0; byte < buffer.length; byte++) {
+        imageBytes[byte] = buffer[byte];
+    }
+    return imageBytes;
+}
+
+function getBufferFromBytes(imageBytes: number[]): Buffer {
+
+    const imageBuffer = Buffer.from(imageBytes);
+    for (let byte = 0; byte < imageBytes.length; byte++) {
+        imageBuffer[byte] = imageBytes[byte];
+    }
+    return imageBuffer;
+}
