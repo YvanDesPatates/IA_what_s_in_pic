c9365e6962e843aa49e1915fdace623ce312a56a
Yvan Roux <yvis99@gmail.com>
2024-07-03 16:40:40 +0200

    get every type of images and return a prediction

--- backend/src/image/ImageController.ts
+++ backend/src/image/ImageController.ts
@@ -6,30 +6,33 @@ import path from "path";
 export class ImageController {
 
     public async predictImage(req: Request, res: Response): Promise<void> {
-
-        if (!req.file) {
+        if (!req.body?.image){
             res.status(400).send("No image uploaded");
             return;
         }
 
-        // Write the buffer to a temporary file to be processed by the Python script
-        const tempImagePath = path.join(__dirname, "../uploads", `${Date.now()}_${req.file.originalname}`);
-        fs.writeFileSync(tempImagePath, req.file.buffer);
+        const base64Image = req.body.image;
+        const binaryData = Buffer.from(base64Image.replace(/^data:[a-zA-Z]+\/[a-zA-Z0-9-.+]*;base64,/, ''), 'base64');
 
-        const outputName = path.join(__dirname, "../compressed", `compressed_${Date.now()}`);
+        // Write the buffer to a temporary file to be processed by the Python script
+        const tempImagePath = path.join(__dirname, "../uploads", `${Date.now()}tmp.webp`);
+        fs.writeFileSync(tempImagePath, binaryData);
+
+        const outputName = 'results.txt';
 
         // Execute the Python compression script
-        exec(`python3 Compression.py "${tempImagePath}" "${outputName}"`, (error, stdout, stderr) => {
+        exec(`python3 Predict.py "${tempImagePath}" "${outputName}"`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
-                res.status(500).send(`Error during compression: ${stderr}`);
+                res.status(500).send(`Error during prediction: ${stderr}`);
                 return;
             }
 
-            res.status(201).send({message: 'Image compressed successfully', outputPath: `${outputName}.webp`});
-
+            // get text from results.txt file
+            const result = fs.readFileSync(outputName, "utf-8");
             // Clean up the temporary image file
             fs.unlinkSync(tempImagePath);
+            res.status(201).send({prediction: result});
         });
 
     }
